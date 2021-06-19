31f4c21d9df5361143fefd888ce50821f2cbff2a
Marco Tribuzio <marco.tribuz@gmail.com>
2021-06-19 20:05:54 +0200

    WIP

--- src/app/search-field/search-field.component.ts
+++ src/app/search-field/search-field.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {ItemService} from "../services/item/item.service";
-import {Observable, of, Subject} from 'rxjs';
-import {
-  debounceTime, distinctUntilChanged, switchMap
-} from 'rxjs/operators';
-import {Item} from "../interfaces/item";
-
 
 @Component({
   selector: 'app-search-field',
@@ -13,29 +7,15 @@ import {Item} from "../interfaces/item";
   styleUrls: ['./search-field.component.css']
 })
 export class SearchFieldComponent implements OnInit {
-  heroes$!: Observable<Item[]>;
-  private searchTerms = new Subject<string>();
 
-  constructor(private heroService: ItemService) {}
-
-  // Push a search term into the observable stream.
-  search(term: string): void {
-    this.searchTerms.next(term);
-  }
+  constructor(private itemService: ItemService) { }
 
   ngOnInit(): void {
-    this.heroes$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
+  }
 
-      // ignore new term if same as previous term
-      distinctUntilChanged(),
-
-      // switch to new search observable each time the term changes
-      switchMap((term: string) => {
-        console.log("Porco", term)
-        return of([])}),
-    );
+  search(value: string): void {
+    console.log("search Field", value)
+    this.itemService.searchItem(value)
   }
 
 }
