d02367fbba13d1008822ec41db197913bc281515
Marco Tribuzio <marco.tribuz@gmail.com>
2021-06-21 11:53:38 +0200

    WIP

---
+++ src/app/favorite-dialog/favorite-dialog.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-dialog',
+  templateUrl: './favorite-dialog.component.html',
+  styleUrls: ['./favorite-dialog.component.css']
+})
+export class FavoriteDialogComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+}
