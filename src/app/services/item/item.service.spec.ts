555f1284f605873d7ee4056bf7c7191e17babe42
Marco Tribuzio <marco.tribuz@gmail.com>
2021-06-23 20:34:19 +0200

    WIP test item service

--- src/app/services/item/item.service.spec.ts
+++ src/app/services/item/item.service.spec.ts
@@ -4,20 +4,21 @@ import {ItemService} from './item.service';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {IItem} from "../../interfaces/IItem";
 
-const itemsList: IItem[] = [{
-  "title": "iPhone 6S Oro",
-  "description": "Vendo un iPhone 6 S color Oro nuevo y sin estrenar. Me han dado uno en el trabajo y no necesito el que me compré. En tienda lo encuentras por 749 euros y yo lo vendo por 740. Las descripciones las puedes encontrar en la web de apple. Esta libre.",
-  "price": "740",
-  "email": "iphonemail@wallapop.com",
-  favorite: false,
-  "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/iphone.png"
-},
+const itemsList: IItem[] = [
+  {
+    "title": "iPhone 6S Oro",
+    "description": "Vendo un iPhone 6 S color Oro nuevo y sin estrenar. Me han dado uno en el trabajo y no necesito el que me compré. En tienda lo encuentras por 749 euros y yo lo vendo por 740. Las descripciones las puedes encontrar en la web de apple. Esta libre.",
+    "price": "740",
+    "email": "iphonemail@wallapop.com",
+    "favorite": false,
+    "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/iphone.png"
+  },
   {
     "title": "Polaroid 635",
     "description": "Cámara clásica de fotos Polaroid, modelo 635. Las fotos son a super color. Está en perfectas condiciones y es fantástica para coleccionistas. Se necesitan carretes instax 20 para hacer fotos. Tamaño M.",
     "price": "50",
     "email": "cameramail@wallapop.com",
-    favorite: false,
+    "favorite": false,
     "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/camera.png"
   },
   {
@@ -25,7 +26,7 @@ const itemsList: IItem[] = [{
     "description": "Vendo bolso de piel marrón grande de la marca Hoss. Lo compré hace dos temporadas. Esta en perfectas condiciones, siempre se ha guardado en bolsa de tela para su conservación. Precio original de 400 euros. Lo vendo por 250 porque ya casi no me lo pongo. Tiene varios compartimentos dentro.",
     "price": "250",
     "email": "bagmail@wallapop.com",
-    favorite: false,
+    "favorite": false,
     "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/bag.png"
   },
   {
@@ -33,7 +34,7 @@ const itemsList: IItem[] = [{
     "description": "Reloj de la marca Daniel Wellington usado sólo un mes. Ahora me han regalado otro que me gusta más y es muy parecido; por eso lo vendo. Su precio en tienda son 149 pero lo vendo por 100 euros. Es con la esfera blanca y la correa de piel marron. ",
     "price": "100",
     "email": "watchmail@wallapop.com",
-    favorite: false,
+    "favorite": false,
     "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/watch.png"
   },
   {
@@ -41,7 +42,7 @@ const itemsList: IItem[] = [{
     "description": "Coche antiguo americano de color marrón. Se tiene que cambiar el motor pero aparte de eso todo funciona correctamente. Interior de piel clara. Ideal para coleccionistas",
     "price": "210000",
     "email": "carmail@wallapop.com",
-    favorite: false,
+    "favorite": false,
     "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/car.png"
   },
   {
@@ -49,7 +50,7 @@ const itemsList: IItem[] = [{
     "description": "Barbacoa en buen estado. La he usado pocas veces y está casi nueva. Ideal para fiestas y celebraciones",
     "price": "120",
     "email": "barbecue@wallapop.com",
-    favorite: false,
+    "favorite": false,
     "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/barbecue.png"
   },
   {
@@ -57,7 +58,7 @@ const itemsList: IItem[] = [{
     "description": "Vendo sofá de piel negro. Tiene signos evidentes de uso, de ahí el precio. Es muy cómodo y bonito",
     "price": "75",
     "email": "sofa@wallapop.com",
-    favorite: false,
+    "favorite": false,
     "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/sofa.png"
   }]
 
@@ -307,6 +308,102 @@ describe('ItemService', () => {
     service.setDefaultItemListBS(itemsList)
     service.sortBy('price', isFavorite, isAscending)
     expect(test).toHaveBeenCalled()
+  });
+
+  it('Should sort by title', async () => {
+    const isFavorite = false
+    const isAscending = true
+
+    const toSort = [
+      {
+        "title": "Polaroid 635",
+        "description": "Cámara clásica de fotos Polaroid, modelo 635. Las fotos son a super color. Está en perfectas condiciones y es fantástica para coleccionistas. Se necesitan carretes instax 20 para hacer fotos. Tamaño M.",
+        "price": "50",
+        "email": "cameramail@wallapop.com",
+        "favorite": false,
+        "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/camera.png"
+      },
+      {
+        "title": "Bolso piel marca Hoss",
+        "description": "Vendo bolso de piel marrón grande de la marca Hoss. Lo compré hace dos temporadas. Esta en perfectas condiciones, siempre se ha guardado en bolsa de tela para su conservación. Precio original de 400 euros. Lo vendo por 250 porque ya casi no me lo pongo. Tiene varios compartimentos dentro.",
+        "price": "250",
+        "email": "bagmail@wallapop.com",
+        "favorite": false,
+        "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/bag.png"
+      },
+      {
+        "title": "Reloj de Daniel Wellington",
+        "description": "Reloj de la marca Daniel Wellington usado sólo un mes. Ahora me han regalado otro que me gusta más y es muy parecido; por eso lo vendo. Su precio en tienda son 149 pero lo vendo por 100 euros. Es con la esfera blanca y la correa de piel marron. ",
+        "price": "100",
+        "email": "watchmail@wallapop.com",
+        "favorite": false,
+        "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/watch.png"
+      },
+      {
+        "title": "Coche antiguo americano",
+        "description": "Coche antiguo americano de color marrón. Se tiene que cambiar el motor pero aparte de eso todo funciona correctamente. Interior de piel clara. Ideal para coleccionistas",
+        "price": "210000",
+        "email": "carmail@wallapop.com",
+        "favorite": false,
+        "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/car.png"
+      },
+      {
+        "title": "Barbacoa",
+        "description": "Barbacoa en buen estado. La he usado pocas veces y está casi nueva. Ideal para fiestas y celebraciones",
+        "price": "120",
+        "email": "barbecue@wallapop.com",
+        "favorite": false,
+        "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/barbecue.png"
+      }]
+
+    const expectedSorted = [
+      {
+        "title": "Barbacoa",
+        "description": "Barbacoa en buen estado. La he usado pocas veces y está casi nueva. Ideal para fiestas y celebraciones",
+        "price": "120",
+        "email": "barbecue@wallapop.com",
+        "favorite": false,
+        "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/barbecue.png"
+      },
+      {
+        "title": "Bolso piel marca Hoss",
+        "description": "Vendo bolso de piel marrón grande de la marca Hoss. Lo compré hace dos temporadas. Esta en perfectas condiciones, siempre se ha guardado en bolsa de tela para su conservación. Precio original de 400 euros. Lo vendo por 250 porque ya casi no me lo pongo. Tiene varios compartimentos dentro.",
+        "price": "250",
+        "email": "bagmail@wallapop.com",
+        "favorite": false,
+        "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/bag.png"
+      },
+      {
+        "title": "Coche antiguo americano",
+        "description": "Coche antiguo americano de color marrón. Se tiene que cambiar el motor pero aparte de eso todo funciona correctamente. Interior de piel clara. Ideal para coleccionistas",
+        "price": "210000",
+        "email": "carmail@wallapop.com",
+        "favorite": false,
+        "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/car.png"
+      },
+      {
+        "title": "Polaroid 635",
+        "description": "Cámara clásica de fotos Polaroid, modelo 635. Las fotos son a super color. Está en perfectas condiciones y es fantástica para coleccionistas. Se necesitan carretes instax 20 para hacer fotos. Tamaño M.",
+        "price": "50",
+        "email": "cameramail@wallapop.com",
+        "favorite": false,
+        "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/camera.png"
+      },
+      {
+        "title": "Reloj de Daniel Wellington",
+        "description": "Reloj de la marca Daniel Wellington usado sólo un mes. Ahora me han regalado otro que me gusta más y es muy parecido; por eso lo vendo. Su precio en tienda son 149 pero lo vendo por 100 euros. Es con la esfera blanca y la correa de piel marron. ",
+        "price": "100",
+        "email": "watchmail@wallapop.com",
+        "favorite": false,
+        "image": "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/watch.png"
+      }]
+    service.setSearchedValue('', isFavorite)
+    service.setStartPosition(5, isFavorite)
+    service.setDefaultItemListBS(toSort)
+    service.setItemListBS(toSort)
+    service.sortBy('title', isFavorite, isAscending)
+    const sortedList = service.getItemListBS()
+    expect(sortedList).toEqual(expectedSorted)
   });
 
 });
